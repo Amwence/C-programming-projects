2f7b6d748101a149d8af5631dba0363473a4e7e3
Amwence <Amwence@users.noreply.github.com>
2023-01-06 23:06:12 -0600

    Comment updates

--- C Projects/name.c
+++ C Projects/name.c
@@ -1,7 +1,12 @@
+// #include is a preprocessor that allows other libraries to be used much like include in python
+
 #include <stdio.h>
 
+// int main(void) is a function and void is a keyword that means the function does not take any arguments
 int main(void)
 {
+    /*printf is like python's print() function and every instruction needs to end with a ; 
+    return 0 is the how to stop the function from running and end the program otherwise it would go on forever.*/
     printf("Hello, Anthony");
     return 0;
 }
