78b82657e0e6d388b19460bc11c2ea4239a7625a
Ding Dong Doodly Duder <Amwence@users.noreply.github.com>
2023-06-01 16:16:04 -0500

    rectangle

---
+++ C-Projects/Rectangle Perimeter and area/Challenge.md
@@ -0,0 +1,15 @@
+# Diplay the perimeter and area of a rectangle
+
+- The program should create 4 variables of type double
+    - one variable to store with width
+    - one variable to store height
+    - one variable to store the perimiter
+    - one variable to store the area
+
+- The program should perform the calculation of the permiter of a rectangle
+    - use the + and * operators
+    - perimiter is height plus width times 2
+    - area is multiplying width and height
+
+- The program should display the height, width, and perimiter in the correct format in one print statement
+
---
+++ C-Projects/Rectangle Perimeter and area/rectangle.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+
+
+//throws a warning without int in front because of c standard 99 still works without int and just main
+int main(){
+    
+    // declare variables and set initial values
+    double rectangleWidth = 0;
+    double rectangleHeight = 0;
+    double rectanglePerimiter = 0;
+    double rectangleArea = 0;
+    //request input for height and width
+    printf("What is the height of the rectangle?\n");
+    scanf("%lf", &rectangleHeight);
+    
+    printf("What is the width of the rectangle?\n");
+    scanf("%lf", &rectangleWidth);
+    // perform calculations for permiter and area
+    rectanglePerimiter = (rectangleWidth + rectangleHeight) * 2;
+    rectangleArea = rectangleWidth * rectangleHeight;
+    //print results
+    printf("The area of the rectangle is: %.2lf\n", rectangleArea); //rounded to the second decimal
+    printf("The perimiter of the rectangle is: %.2lf\n", rectanglePerimiter); // rounded to the second decimal
+    printf(" The AREA is %f ::: The PERIMITER is %f ::: The HEIGHT is %f ::: The WIDTH is %f\n", rectangleArea, rectanglePerimiter, rectangleHeight, rectangleWidth); // all in one print statement
+    printf(" The AREA is %f \n The PERIMITER is %f \n The HEIGHT is %f \n The WIDTH is %f\n", rectangleArea, rectanglePerimiter, rectangleHeight, rectangleWidth); // all in one print statement on new lines
+    
+    return 0;
+}
